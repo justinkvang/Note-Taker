4ac7e4b9416ea1e14c74d2e6f3342e3758b88022
JMan <justinvang07@gmail.com>
2021-02-03 19:44:23 -0600

    updated codes to one routes.js, corrected code in server.js

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ var express = require("express");
 // tells node that I am creating an express server 
 var app = express();
 // sets an initial port that will be used in listener 
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3000;
 
 // sets up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// requiring these route files 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// requiring these route file
+require("./routes/routes.js")(app);
 
 // listener to start our server 
 app.listen(PORT, function() {
