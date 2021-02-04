697953119d95ca4068ddf14e634586f07a7b9d79
JMan <justinvang07@gmail.com>
2021-02-03 21:27:19 -0600

    updated a bunch of files

--- routes/routes.js
+++ routes/routes.js
@@ -5,19 +5,19 @@ module.exports = app => {
 
     // Setup notes variable
     fs.readFile("db/db.json","utf8", (err, data) => {
+
         if (err) throw err;
+
         var notes = JSON.parse(data);
 
-        // API ROUTEs
+        // API ROUTES
         // Setup the /api/notes get route
         app.get("/api/notes", function(req, res) {
-            // Read the db.json file and return all saved notes as JSON.
             res.json(notes);
         });
 
         // Setup the /api/notes post route
         app.post("/api/notes", function(req, res) {
-            // Receives a new note, adds it to db.json, then returns the new note
             let newNote = req.body;
             notes.push(newNote);
             updateDb();
@@ -26,7 +26,6 @@ module.exports = app => {
 
         // Retrieves a note with specific id
         app.get("/api/notes/:id", function(req,res) {
-            // display json for the notes array indices of the provided id
             res.json(notes[req.params.id]);
         });
 
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-// requiring express
+// Require Dependencies
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
-// tells node that I am creating an express server 
+const path = require('path');
+
+// Initialize express app
 const app = express();
-// sets an initial port that will be used in listener 
 const PORT = process.env.PORT || 3000;
 
-// sets up the express app to handle data parsing
+// Setup data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
-// requiring these route file
-require("./routes/routes")(app);
+//Require routes file
+require('./routes/routes')(app);
 
-// listener to start our server 
+// Setup listener
 app.listen(PORT, function() {
-    console.log("App listening localhost: " + PORT);
-  });
+    console.log("App listening on PORT: " + PORT);
+});  
